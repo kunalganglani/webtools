9356619b30eb7ef3a733c758084c061a9cf85ea0
Kunal Ganglani <kunalganglani@gmail.com>
2018-07-08 12:04:48 +0530

    dynamic load function added and assets added

--- src/app/css-web-tool/css-minify/css-minify.component.ts
+++ src/app/css-web-tool/css-minify/css-minify.component.ts
@@ -6,8 +6,51 @@ import { Component, OnInit, SystemJsNgModuleLoader } from '@angular/core';
 })
 export class CssMinifyComponent implements OnInit {
 
+  loadAPI: Promise<any>;
+
   constructor() {
-   }
+    this.loadScripts();
+      // this.loadAPI = new Promise((resolve) => {
+      //     this.loadScript();
+      //     resolve(true);
+      // });
+  }
+  loadScripts() {
+    const dynamicScripts = ['https://widgets.skyscanner.net/widget-server/js/loader.js'];
+    for (let i = 0; i < dynamicScripts.length; i++) {
+      const node = document.createElement('script');
+      node.type = 'text/javascript';
+      node.src = dynamicScripts[i];
+      node.async = false;
+      node.charset = 'utf-8';
+      document.getElementsByTagName('head')[0].appendChild(node);
+    }
+  }
+
+
+  // public loadScript() {
+  //     let isFound = false;
+  //     const scripts = document.getElementsByTagName('script')
+  //     for (let i = 0; i < scripts.length; ++i) {
+  //         if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
+  //             isFound = true;
+  //         }
+  //     }
+
+  //     if (!isFound) {
+  //         const dynamicScripts = ['https://widgets.skyscanner.net/widget-server/js/loader.js'];
+
+  //         for (let i = 0; i < dynamicScripts .length; i++) {
+  //             const node = document.createElement('script');
+  //             node.src = dynamicScripts [i];
+  //             node.type = 'text/javascript';
+  //             node.async = false;
+  //             node.charset = 'utf-8';
+  //             document.getElementsByTagName('head')[0].appendChild(node);
+  //         }
+
+  //     }
+  // }
 
   ngOnInit() {
   }
@@ -23,9 +66,9 @@ export class CssMinifyComponent implements OnInit {
     // require ('../../../assets/cleanCssJS/dragDrop.js');
     // require ('../../../assets/cleanCssJS/settings.js');
 
-    // declare var optimizer: any;
-    // declare var dragDrop: any;
-    // declare var settings: any;
+    // declare let optimizer: any;
+    // declare let dragDrop: any;
+    // declare let settings: any;
   // import { Optimizer, dragDrop, settings } from './dragDrop';
 
   // loadScripts() {
