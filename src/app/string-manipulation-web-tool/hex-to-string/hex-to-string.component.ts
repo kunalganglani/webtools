76be996a098291aca51c1973bc0047c408d09355
Kunal Ganglani <kunalganglani@gmail.com>
2018-12-23 19:38:55 +0530

    string-hex done

--- src/app/string-manipulation-web-tool/hex-to-string/hex-to-string.component.ts
+++ src/app/string-manipulation-web-tool/hex-to-string/hex-to-string.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HexToStringComponent implements OnInit {
 
+  outputStr = '';
   constructor() { }
 
   ngOnInit() {
+  }
+  hexToString(strInput) {
+    this.outputStr = this.hexDecode(strInput);
+  }
+
+  hexDecode(strInput) {
+    let j;
+    const hexes = strInput.match(/.{1,4}/g) || [];
+    let back = '';
+    for (j = 0; j < hexes.length; j++) {
+        back += String.fromCharCode(parseInt(hexes[j], 16));
+    }
+
+    return back;
   }
 
 }
--- src/app/string-manipulation-web-tool/string-to-hex/string-to-hex.component.ts
+++ src/app/string-manipulation-web-tool/string-to-hex/string-to-hex.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StringToHexComponent implements OnInit {
 
+  outputStr = '';
   constructor() { }
 
   ngOnInit() {
+  }
+  stringToHex(strInput) {
+    this.outputStr = this.hexEncode(strInput);
+  }
+
+  hexEncode(strInput) {
+    let hex, i;
+    let result = '';
+    for (i = 0; i < strInput.length; i++) {
+        hex = strInput.charCodeAt(i).toString(16);
+        result += ('000' + hex).slice(-4);
+    }
+    return result;
   }
 
 }
