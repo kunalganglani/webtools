252c5f3f74e5fd3f66d454fbba9bafc383ef7c48
Kunal Ganglani <kunalganglani@gmail.com>
2018-12-24 14:43:31 +0530

    password generator done

--- src/app/password-web-tool/password-generator/password-generator.component.ts
+++ src/app/password-web-tool/password-generator/password-generator.component.ts
@@ -8,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class PasswordGeneratorComponent implements OnInit {
 
   constructor() { }
-
+  defaultCharset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  defaultLength = 8;
+  outputStr = '';
   ngOnInit() {
+  }
+
+  generatePassword(charset, length) {
+    let retVal = '';
+    if (length === '') {
+      length = this.defaultLength;
+    }
+    if (charset === '') {
+      charset = this.defaultCharset;
+    }
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    this.outputStr = retVal;
   }
 
 }
